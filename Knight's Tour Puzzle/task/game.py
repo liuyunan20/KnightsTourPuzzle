e9f38eb278fa810fa357277dab6059577639f532
liuyunan <liuyunan.20@gmail.com>
2021-12-26 16:59:56 -0800

    User can play the game. Check if user input move is available. Check if the game is over.

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -63,13 +63,14 @@ def start_board(position, dimension):
     return board
 
 
-def build_board(position, cur_board):
-    x = position[0]
-    y = position[1]
+def display_board(cur_position, cur_board):
+    dp_board = [[x for x in y] for y in cur_board]
+    x = cur_position[0]
+    y = cur_position[1]
     m = len(cur_board[0])
     n = len(cur_board)
-    predict_move(position, cur_board)
-    return cur_board
+    predict_move(cur_position, dp_board)
+    return dp_board
 
 
 def predict_move(cur_position, cur_board):
@@ -86,7 +87,7 @@ def check_move_available(position, cur_board):
     y = position[1]
     m = len(cur_board[0])
     n = len(cur_board)
-    if 1 <= x <= m and 1 <= y <= n and cur_board[y - 1][x - 1] == "__":
+    if 1 <= x <= m and 1 <= y <= n and cur_board[y - 1][x - 1] != " X" and cur_board[y - 1][x - 1] != " *":
         return True
     else:
         return False
@@ -103,12 +104,70 @@ def possible_moves(cur_position, cur_board):
                      [x - 2, y - 1],
                      [x - 2, y + 1],
                      [x - 1, y + 2]]
-    return [move for move in possible_move if check_move_available(move, cur_board)]
+    moves = [move for move in possible_move if check_move_available(move, cur_board)]
+    return moves
+
+
+def process_move(pre_position, cur_board):
+    while True:
+        cur_position = input("Enter your next move: ")
+        cur_position = check_type(cur_position, "move")
+        if cur_position is None:
+            continue
+        elif cur_position in possible_moves(pre_position, cur_board):
+            return cur_position
+        else:
+            print_invalid("move")
+
+
+def update_board(cur_position, cur_board):
+    for y in range(len(cur_board)):
+        for x in range(len(cur_board[0])):
+            if cur_board[y][x] == " X":
+                cur_board[y][x] = " *"
+    x = cur_position[0]
+    y = cur_position[1]
+    cur_board[y - 1][x - 1] = " X"
+    return cur_board
+
+
+def count_moves(cur_board):
+    steps = 0
+    for y in cur_board:
+        for x in y:
+            if x == " X" or x == " *":
+                steps += 1
+    return steps
+
+
+def check_win(cur_board):
+    for y in cur_board:
+        if "__" in y:
+            return False
+    return True
+
+
+def check_end(cur_position, cur_board):
+    moves = possible_moves(cur_position, cur_board)
+    if check_win(cur_board):
+        print("What a great tour! Congratulations!")
+        return True
+    elif not moves:
+        print("No more possible moves!")
+        print(f"Your knight visited {count_moves(cur_board)} squares!")
+        return True
+    else:
+        return False
 
 
 dimension_in = check_dimension()
-position_in = check_position(dimension_in)
-s_board = start_board(position_in, dimension_in)
-c_board = build_board(position_in, s_board)
-print("Here are the possible moves:")
-print_board(c_board)
+c_position = check_position(dimension_in)
+c_board = start_board(c_position, dimension_in)
+while True:
+    d_board = display_board(c_position, c_board)
+    print_board(d_board)
+    p_position = [x for x in c_position]
+    c_position = process_move(p_position, c_board)
+    c_board = update_board(c_position, c_board)
+    if check_end(c_position, c_board):
+        break
